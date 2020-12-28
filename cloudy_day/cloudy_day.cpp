0dc3cd450541edea5694b1b3d9c34c34515764b0
kostyadev <kostya.demchenko@gmail.com>
2020-12-28 14:06:01 +0200

    add files

---
+++ cloudy_day/cloudy_day.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Complete the maximumPeople function below.
+long maximumPeople(const vector<long>& p, const vector<long>& x, const vector<long>& y, const vector<long>& r)
+{
+	// Return the maximum number of people that will be in a sunny town after removing exactly one cloud.
+	return 0;
+}
+
+int main()
+{
+	vector<long> p = { 10, 100 };
+	vector<long> x = { 5, 100 };
+	vector<long> y = { 4 };
+	vector<long> r = { 1 };
+	long result = maximumPeople(p, x, y, r);
+	cout << result << "\n";
+}
